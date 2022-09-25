614836067338eace81eeb520e8e515e215572623
andykuo1 <andykuo1supergreen@gmail.com>
2022-09-25 09:56:01 -0700

    Remove deprecated code

--- apps/webapp/src/app/deprecated/system/notification/Notifications.jsx
+++
@@ -1,5 +0,0 @@
-import NotificationManager from './NotificationManager';
-
-//Singleton pattern (I know, I'm sorry. But we must.)
-const INSTANCE = new NotificationManager();
-export default INSTANCE;
