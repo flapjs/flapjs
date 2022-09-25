b90f921af755a9a5c22a09c3b27714b5eb18cd6e
andykuo1 <andykuo1supergreen@gmail.com>
2022-09-24 21:39:42 -0700

    Fix missing root base path for dist

--- apps/webapp/vite.config.js
+++ apps/webapp/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/flapjs/',
   resolve: {
     alias: [
       { find: 'src', replacement: path.resolve('.', 'src') },
