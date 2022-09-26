4ff2cf54db1ffbf718830f330f385418c156a77a
andykuo1 <andykuo1supergreen@gmail.com>
2022-09-25 22:59:38 -0700

    Fix css modules type declarations

--- apps/webapp/declarations.d.ts
+++ apps/webapp/declarations.d.ts
@@ -1,9 +1,13 @@
-import React from 'react';
-
-declare module "*.module.css" {
+declare module '*.module.css' {
     const content: Record<string, string>;
     export default content;
 }
+
+declare module '*.css' {
+    const content: string;
+    export default content;
+}
+
 declare module '*.svg' {
     const content: React.FC<React.SVGProps<SVGElement>>;
     export default content;
